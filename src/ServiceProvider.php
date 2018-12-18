64f556e7c8fa211fb19316bbd393dd94b26189f7
Mingwei Peng <mingw.peng@gmail.com>
2018-12-18 11:12:43 +1100

    register Firestore as singleton

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,7 +1,26 @@
 <?php
 namespace Mwpeng\Debugbar;
 
+use Kreait\Firebase\ServiceAccount;
+use Morrislaptop\Firestore\Factory;
+use Morrislaptop\Firestore\Firestore;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton(Firestore::class, function ($app) {
+            $serviceAccount = ServiceAccount::fromJsonFile(env("FIREBASE_CREDENTIALS"));
+            $firestore = (new Factory)->withServiceAccount($serviceAccount)->createFirestore();
+            return $firestore;
+        });
+
+    }
 }
