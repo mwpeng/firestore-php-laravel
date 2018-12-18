c53eb7f60fea676307345a900599811add172a39
Mingwei Peng <mingw.peng@gmail.com>
2018-12-18 16:49:57 +1100

    place holder for config file

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,6 +9,19 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
     /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        $this->publishes([
+//            __DIR__.'/path/to/config/courier.php' => config_path('courier.php'),
+//        ]);
+    }
+
+
+    /**
      * Register bindings in the container.
      *
      * @return void
